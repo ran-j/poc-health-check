38a8e2dbb42af2f1ef52145deec490e15747b205
Ran-j <ran-junior@hotmail.com>
2023-05-17 15:49:23 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,73 @@
+//create a express app
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const HealthCheckModule = require('./lib/health-check');
+const app = express();
+const port = 3000;
+
+///////////////DB /////////////////////
+mongoose.connect(
+    'mongodb://localhost:27017/poc-healthcheck',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+).then(() => {
+    console.log('MongoDB connected…');
+}).catch((err) => {
+    console.log(err);
+});
+
+//Model
+const Book = mongoose.model('Book', { name: String });
+
+///////////////Server /////////////////////
+
+const HealthCheck = new HealthCheckModule();
+//register the integration
+HealthCheck.registerIntegration('mongodb', 'database');
+HealthCheck.registerIntegration('pokemon', 'api');
+
+app.use(bodyParser.json());
+
+//create a route for the app
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/book', async (req, res) => {
+    const { name } = req.body;
+
+    try {
+        //this is to simulate an error
+        if (name === 'error') throw new Error('Error');
+
+        const book = new Book({ name });
+        await book.save();
+        res.send('Book saved');
+    } catch (error) {
+        HealthCheck.incrementIntegrationError('mongodb');
+        res.status(500).send('Error');
+    }
+});
+
+app.get('/pokemon/:name', async (req, res) => {
+    const { name } = req.params;
+
+    try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        res.json(response.data);
+    } catch (error) {
+        HealthCheck.incrementIntegrationError('pokemon');
+        res.status(500).send('Error');
+    }
+});
+
+app.get('/health', async (req, res) => {
+    const result = await HealthCheck.getResponse();
+    res.json(result);
+})
+
+//listen to the port
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ src/lib/health-check/index.js
@@ -0,0 +1,123 @@
+// interface HealthCheckModule {
+//     determineStatus(): Status;
+//     getResponse(): HealthCheckResponse;
+//     checkIntegrations(): Promise<Integration[]>;
+//     registerIntegration(name: string, kind: string, optional?: boolean): void;
+//     incrementIntegrationError(name: string): void;
+//     resetAllIntegrationErrors(): void;
+// }
+
+
+module.exports = class HealthCheckModule {
+
+    constructor() {
+        this.integrationsMap = new Map();
+    }
+
+    registerIntegration(name, kind, config = {}, optional = false) {
+        config = { ...this._getDefaultConfig(), ...config };
+        this.integrationsMap.set(name, { kind, optional, errors: [], status: 'pass', config, name });
+    }
+
+    incrementIntegrationError(name) {
+        const integration = this.integrationsMap.get(name);
+        if (integration) {
+            integration.errors.push({
+                timestamp: Date.now()
+            });
+        }
+    }
+
+    setIntegrationStatus(name, status) {
+        const integration = this.integrationsMap.get(name);
+        if (integration) {
+            integration.status = status;
+        }
+    }
+
+    async getResponse() {
+        return {
+            status: await this._determineStatus(),
+            integrations: Array.from(this.integrationsMap.values())
+        };
+    }
+
+    async _determineStatus() {
+        await this._checkIntegrations();
+
+        const integrationArray = Array.from(this.integrationsMap.values());
+
+        if (integrationArray.some(integration => integration.status === 'fail' && !integration.optional)) {
+            return 'down';
+        }
+
+        if (integrationArray.some(integration => integration.status === 'warn' && !integration.optional)) {
+            return 'unhealthy';
+        }
+
+        return "healthy";
+    }
+
+    _checkIntegrations() {
+        const promises = [];
+        for (const integration of this.integrationsMap.values()) {
+            promises.push(this._checkIntegration(integration));
+        }
+        return Promise.all(promises);
+    }
+
+    resetAllIntegrationErrors() {
+        for (const integration of this.integrationsMap.values()) {
+            integration.errors = [];
+        }
+    }
+
+    _checkIntegration(integration) {
+        return new Promise((resolve) => {
+            const { errorPerMinuteToFailState, errorPerMinuteToWarnState } = integration.config;
+
+            this.setIntegrationStatus(integration.name, 'pass');
+
+            if (integration.errors.length === 0) {
+                return resolve();
+            }
+
+            const totalErrorInMs = integration.errors[0].timestamp - integration.errors[integration.errors.length - 1].timestamp;
+            const totalErrorInMinutes = totalErrorInMs / 60000;
+
+            const errorCount = integration.errors.length;
+            const errorPerMinute = errorCount / Math.max(totalErrorInMinutes, 1);
+
+            if (errorPerMinuteToFailState !== -1) {
+                if (errorPerMinute >= errorPerMinuteToFailState) {
+                    this.setIntegrationStatus(integration.name, 'fail');
+                    return resolve();
+                }
+
+                if (errorPerMinuteToFailState === 0 && errorCount > 0) {
+                    this.setIntegrationStatus(integration.name, 'fail');
+                    return resolve();
+                }
+            }
+
+            if (errorPerMinuteToWarnState !== -1) {
+                if (errorPerMinute >= errorPerMinuteToWarnState) {
+                    this.setIntegrationStatus(integration.name, 'warn');
+                    return resolve();
+                }
+                if (errorPerMinuteToWarnState === 0 && errorCount > 0) {
+                    this.setIntegrationStatus(integration.name, 'warn');
+                }
+            }
+
+            return resolve();
+        });
+    }
+
+    _getDefaultConfig() {
+        return {
+            errorPerMinuteToFailState: 5, //0 means any, --1 means never
+            errorPerMinuteToWarnState: 0, //0 means any, -1 means never
+        };
+    }
+}
